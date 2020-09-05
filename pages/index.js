fb7cda36b1e06880f2a53144f423cdcb29b5cceb
Rakesh Rankawat <rakesh.rankawat@outlook.com>
2020-09-05 16:31:39 +0530

    bootstarp & fetch

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,20 @@
-import Navbar from "../components/Navbar";
+import Fetch from "isomorphic-unfetch";
+import Layout from "../components/Layout";
 
-const Index = () => (
-  <div>
-    <Navbar />
+const Index = (props) => (
+  <Layout>
     <h1>Welcome To BitzPrice</h1>
-  </div>
+    {props.bpi.time.updated}
+  </Layout>
 );
+
+Index.getInitialProps = async () => {
+  const res = await fetch(`https://api.coindesk.com/v1/bpi/currentprice.json`);
+  const data = await res.json();
+
+  return {
+    bpi: data,
+  };
+};
 
 export default Index;
